849e8db8b4833b9544fa997940eb2348f5e13c76
luna0228 <51066795+luna0228@users.noreply.github.com>
2023-12-04 10:11:08 +0800

    Update database.py

--- db/database.py
+++ db/database.py
@@ -4,6 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./homework.db"
 # for github (postgresql)
+#host=> ep-red-dawn-83306951.us-east-1.postgres.vercel-storage.com
+#user=> default
+#password=> 8RUjDA7dBQoC
+#database=> verceldb
 SQLALCHEMY_DATABASE_URL = "postgresql://default:8RUjDA7dBQoC@ep-red-dawn-83306951.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 
 engine = create_engine(
