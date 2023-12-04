70fe282323e33a810afc1c512c86b8d6ce08adc3
luna0228 <51066795+luna0228@users.noreply.github.com>
2023-12-04 09:41:43 +0800

    update

--- db/database.py
+++ db/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./homework.db"
-# for github
-SQLALCHEMY_DATABASE_URL = "postgres://default:8RUjDA7dBQoC@ep-red-dawn-83306951.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+# for github (postgresql)
+SQLALCHEMY_DATABASE_URL = "postgresql://default:8RUjDA7dBQoC@ep-red-dawn-83306951.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
